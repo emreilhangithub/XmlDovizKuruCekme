275306a2582a09760b906d4e396ac20fef740ad3
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-08-27 21:13:09 +0300

    Döviz kurları çekilerek labellere yazdırıldı

--- XmlDovizKuruCekme/Form1.Designer.cs
+++ XmlDovizKuruCekme/Form1.Designer.cs
@@ -29,13 +29,335 @@ namespace XmlDovizKuruCekme
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblUsdCurrency = new System.Windows.Forms.Label();
+            this.lblBuyingUsd = new System.Windows.Forms.Label();
+            this.btnDovizKuruCek = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSellingUsd = new System.Windows.Forms.Label();
+            this.lblUnitUsd = new System.Windows.Forms.Label();
+            this.lblUnitEuro = new System.Windows.Forms.Label();
+            this.lblSellingEuro = new System.Windows.Forms.Label();
+            this.lblBuyingEuro = new System.Windows.Forms.Label();
+            this.lblEuroCurrency = new System.Windows.Forms.Label();
+            this.lblUnitGbp = new System.Windows.Forms.Label();
+            this.lblSellingStr = new System.Windows.Forms.Label();
+            this.lblBuyingSter = new System.Windows.Forms.Label();
+            this.lblGbpCurrency = new System.Windows.Forms.Label();
+            this.lblUnitSar = new System.Windows.Forms.Label();
+            this.lblSellingSar = new System.Windows.Forms.Label();
+            this.lblBuyingSar = new System.Windows.Forms.Label();
+            this.lblSarCurrency = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUsdCurrency
+            // 
+            this.lblUsdCurrency.AutoSize = true;
+            this.lblUsdCurrency.Location = new System.Drawing.Point(375, 106);
+            this.lblUsdCurrency.Name = "lblUsdCurrency";
+            this.lblUsdCurrency.Size = new System.Drawing.Size(51, 20);
+            this.lblUsdCurrency.TabIndex = 1;
+            this.lblUsdCurrency.Text = "label2";
+            // 
+            // lblBuyingUsd
+            // 
+            this.lblBuyingUsd.AutoSize = true;
+            this.lblBuyingUsd.Location = new System.Drawing.Point(559, 106);
+            this.lblBuyingUsd.Name = "lblBuyingUsd";
+            this.lblBuyingUsd.Size = new System.Drawing.Size(51, 20);
+            this.lblBuyingUsd.TabIndex = 2;
+            this.lblBuyingUsd.Text = "label3";
+            // 
+            // btnDovizKuruCek
+            // 
+            this.btnDovizKuruCek.Location = new System.Drawing.Point(397, 330);
+            this.btnDovizKuruCek.Name = "btnDovizKuruCek";
+            this.btnDovizKuruCek.Size = new System.Drawing.Size(170, 71);
+            this.btnDovizKuruCek.TabIndex = 3;
+            this.btnDovizKuruCek.Text = "Döviz Kuru Çek";
+            this.btnDovizKuruCek.UseVisualStyleBackColor = true;
+            this.btnDovizKuruCek.Click += new System.EventHandler(this.btnDovizKuruCek_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(198, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Döviz Kodu Currency Code";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(243, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Birim Unit";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(375, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Döviz Cinsi Currency\t";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(559, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(173, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Döviz Alış Forex Buying";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(28, 106);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 20);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "USD/TRY";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(28, 150);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(80, 20);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "EUR/TRY";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(28, 203);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(79, 20);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "GBP/TRY";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(28, 257);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(79, 20);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "SAR/TRY";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(755, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Döviz Satış Forex Selling";
+            // 
+            // lblSellingUsd
+            // 
+            this.lblSellingUsd.AutoSize = true;
+            this.lblSellingUsd.Location = new System.Drawing.Point(755, 106);
+            this.lblSellingUsd.Name = "lblSellingUsd";
+            this.lblSellingUsd.Size = new System.Drawing.Size(60, 20);
+            this.lblSellingUsd.TabIndex = 12;
+            this.lblSellingUsd.Text = "label12";
+            // 
+            // lblUnitUsd
+            // 
+            this.lblUnitUsd.AutoSize = true;
+            this.lblUnitUsd.Location = new System.Drawing.Point(243, 106);
+            this.lblUnitUsd.Name = "lblUnitUsd";
+            this.lblUnitUsd.Size = new System.Drawing.Size(60, 20);
+            this.lblUnitUsd.TabIndex = 13;
+            this.lblUnitUsd.Text = "label13";
+            // 
+            // lblUnitEuro
+            // 
+            this.lblUnitEuro.AutoSize = true;
+            this.lblUnitEuro.Location = new System.Drawing.Point(243, 150);
+            this.lblUnitEuro.Name = "lblUnitEuro";
+            this.lblUnitEuro.Size = new System.Drawing.Size(60, 20);
+            this.lblUnitEuro.TabIndex = 17;
+            this.lblUnitEuro.Text = "label14";
+            // 
+            // lblSellingEuro
+            // 
+            this.lblSellingEuro.AutoSize = true;
+            this.lblSellingEuro.Location = new System.Drawing.Point(755, 150);
+            this.lblSellingEuro.Name = "lblSellingEuro";
+            this.lblSellingEuro.Size = new System.Drawing.Size(60, 20);
+            this.lblSellingEuro.TabIndex = 16;
+            this.lblSellingEuro.Text = "label15";
+            // 
+            // lblBuyingEuro
+            // 
+            this.lblBuyingEuro.AutoSize = true;
+            this.lblBuyingEuro.Location = new System.Drawing.Point(559, 150);
+            this.lblBuyingEuro.Name = "lblBuyingEuro";
+            this.lblBuyingEuro.Size = new System.Drawing.Size(60, 20);
+            this.lblBuyingEuro.TabIndex = 15;
+            this.lblBuyingEuro.Text = "label16";
+            // 
+            // lblEuroCurrency
+            // 
+            this.lblEuroCurrency.AutoSize = true;
+            this.lblEuroCurrency.Location = new System.Drawing.Point(375, 150);
+            this.lblEuroCurrency.Name = "lblEuroCurrency";
+            this.lblEuroCurrency.Size = new System.Drawing.Size(60, 20);
+            this.lblEuroCurrency.TabIndex = 14;
+            this.lblEuroCurrency.Text = "label17";
+            // 
+            // lblUnitGbp
+            // 
+            this.lblUnitGbp.AutoSize = true;
+            this.lblUnitGbp.Location = new System.Drawing.Point(243, 203);
+            this.lblUnitGbp.Name = "lblUnitGbp";
+            this.lblUnitGbp.Size = new System.Drawing.Size(60, 20);
+            this.lblUnitGbp.TabIndex = 21;
+            this.lblUnitGbp.Text = "label18";
+            // 
+            // lblSellingStr
+            // 
+            this.lblSellingStr.AutoSize = true;
+            this.lblSellingStr.Location = new System.Drawing.Point(755, 203);
+            this.lblSellingStr.Name = "lblSellingStr";
+            this.lblSellingStr.Size = new System.Drawing.Size(60, 20);
+            this.lblSellingStr.TabIndex = 20;
+            this.lblSellingStr.Text = "label19";
+            // 
+            // lblBuyingSter
+            // 
+            this.lblBuyingSter.AutoSize = true;
+            this.lblBuyingSter.Location = new System.Drawing.Point(559, 203);
+            this.lblBuyingSter.Name = "lblBuyingSter";
+            this.lblBuyingSter.Size = new System.Drawing.Size(60, 20);
+            this.lblBuyingSter.TabIndex = 19;
+            this.lblBuyingSter.Text = "label20";
+            // 
+            // lblGbpCurrency
+            // 
+            this.lblGbpCurrency.AutoSize = true;
+            this.lblGbpCurrency.Location = new System.Drawing.Point(375, 203);
+            this.lblGbpCurrency.Name = "lblGbpCurrency";
+            this.lblGbpCurrency.Size = new System.Drawing.Size(60, 20);
+            this.lblGbpCurrency.TabIndex = 18;
+            this.lblGbpCurrency.Text = "label21";
+            // 
+            // lblUnitSar
+            // 
+            this.lblUnitSar.AutoSize = true;
+            this.lblUnitSar.Location = new System.Drawing.Point(243, 257);
+            this.lblUnitSar.Name = "lblUnitSar";
+            this.lblUnitSar.Size = new System.Drawing.Size(60, 20);
+            this.lblUnitSar.TabIndex = 25;
+            this.lblUnitSar.Text = "label22";
+            // 
+            // lblSellingSar
+            // 
+            this.lblSellingSar.AutoSize = true;
+            this.lblSellingSar.Location = new System.Drawing.Point(755, 257);
+            this.lblSellingSar.Name = "lblSellingSar";
+            this.lblSellingSar.Size = new System.Drawing.Size(60, 20);
+            this.lblSellingSar.TabIndex = 24;
+            this.lblSellingSar.Text = "label23";
+            // 
+            // lblBuyingSar
+            // 
+            this.lblBuyingSar.AutoSize = true;
+            this.lblBuyingSar.Location = new System.Drawing.Point(559, 257);
+            this.lblBuyingSar.Name = "lblBuyingSar";
+            this.lblBuyingSar.Size = new System.Drawing.Size(60, 20);
+            this.lblBuyingSar.TabIndex = 23;
+            this.lblBuyingSar.Text = "label24";
+            // 
+            // lblSarCurrency
+            // 
+            this.lblSarCurrency.AutoSize = true;
+            this.lblSarCurrency.Location = new System.Drawing.Point(375, 257);
+            this.lblSarCurrency.Name = "lblSarCurrency";
+            this.lblSarCurrency.Size = new System.Drawing.Size(60, 20);
+            this.lblSarCurrency.TabIndex = 22;
+            this.lblSarCurrency.Text = "label25";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(949, 417);
+            this.Controls.Add(this.lblUnitSar);
+            this.Controls.Add(this.lblSellingSar);
+            this.Controls.Add(this.lblBuyingSar);
+            this.Controls.Add(this.lblSarCurrency);
+            this.Controls.Add(this.lblUnitGbp);
+            this.Controls.Add(this.lblSellingStr);
+            this.Controls.Add(this.lblBuyingSter);
+            this.Controls.Add(this.lblGbpCurrency);
+            this.Controls.Add(this.lblUnitEuro);
+            this.Controls.Add(this.lblSellingEuro);
+            this.Controls.Add(this.lblBuyingEuro);
+            this.Controls.Add(this.lblEuroCurrency);
+            this.Controls.Add(this.lblUnitUsd);
+            this.Controls.Add(this.lblSellingUsd);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnDovizKuruCek);
+            this.Controls.Add(this.lblBuyingUsd);
+            this.Controls.Add(this.lblUsdCurrency);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Döviz Kuru Çekme";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label lblUsdCurrency;
+        private System.Windows.Forms.Label lblBuyingUsd;
+        private System.Windows.Forms.Button btnDovizKuruCek;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSellingUsd;
+        private System.Windows.Forms.Label lblUnitUsd;
+        private System.Windows.Forms.Label lblUnitEuro;
+        private System.Windows.Forms.Label lblSellingEuro;
+        private System.Windows.Forms.Label lblBuyingEuro;
+        private System.Windows.Forms.Label lblEuroCurrency;
+        private System.Windows.Forms.Label lblUnitGbp;
+        private System.Windows.Forms.Label lblSellingStr;
+        private System.Windows.Forms.Label lblBuyingSter;
+        private System.Windows.Forms.Label lblGbpCurrency;
+        private System.Windows.Forms.Label lblUnitSar;
+        private System.Windows.Forms.Label lblSellingSar;
+        private System.Windows.Forms.Label lblBuyingSar;
+        private System.Windows.Forms.Label lblSarCurrency;
     }
 }
 
--- XmlDovizKuruCekme/Form1.cs
+++ XmlDovizKuruCekme/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml; //xml çağırdık
 
 namespace XmlDovizKuruCekme
 {
@@ -15,6 +16,57 @@ namespace XmlDovizKuruCekme
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnDovizKuruCek_Click(object sender, EventArgs e)
+        {
+            string bugun = "https://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmldoc = new XmlDocument();//xmldöküman nesnesi oluşturduk
+            xmldoc.Load(bugun); //bugunü yükledik
+            DateTime tarih = Convert.ToDateTime(xmldoc.SelectSingleNode("//Tarih_Date").Attributes["Tarih"].Value);
+
+            string usdUnit = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='USD']/Unit").InnerXml;
+            lblUnitUsd.Text = string.Format("{0}", usdUnit);
+            string usdIsim = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='USD']/Isim").InnerXml;
+            lblUsdCurrency.Text = string.Format("{0}", usdIsim);
+            string usdDövizAlis = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='USD']/ForexBuying").InnerXml;
+            lblBuyingUsd.Text = string.Format("{0}", usdDövizAlis);
+            string usdDövizSatis = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='USD']/ForexSelling").InnerXml;
+            lblSellingUsd.Text = string.Format("{0}", usdDövizSatis);
+
+            string euroUnit = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='EUR']/Unit").InnerXml;
+            lblUnitEuro.Text = string.Format("{0}", euroUnit);
+            string euroIsim = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='EUR']/Isim").InnerXml;
+            lblEuroCurrency.Text = string.Format("{0}", euroIsim);
+            string euroDövizAlis = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='EUR']/ForexBuying").InnerXml;
+            lblBuyingEuro.Text = string.Format("{0}", euroDövizAlis);
+            string euroDövizSatis = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='EUR']/ForexSelling").InnerXml;
+            lblSellingEuro.Text = string.Format("{0}", euroDövizSatis);
+
+            string gbpUnit = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='GBP']/Unit").InnerXml;
+            lblUnitGbp.Text = string.Format("{0}", gbpUnit);
+            string gbpIsim = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='GBP']/Isim").InnerXml;
+            lblGbpCurrency.Text = string.Format("{0}", gbpIsim);
+            string gbpDövizAlis = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='GBP']/ForexBuying").InnerXml;
+            lblBuyingSter.Text = string.Format("{0}", gbpDövizAlis);
+            string gbpDövizSatis = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='GBP']/ForexSelling").InnerXml;
+            lblSellingStr.Text = string.Format("{0}", gbpDövizSatis);
+
+            string sarUnit = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='SAR']/Unit").InnerXml;
+            lblUnitSar.Text = string.Format("{0}", sarUnit);
+            string sarIsim = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='SAR']/Isim").InnerXml;
+            lblSarCurrency.Text = string.Format("{0}", sarIsim);
+            string sarDövizAlis = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='SAR']/ForexBuying").InnerXml;
+            lblBuyingSar.Text = string.Format("{0}", sarDövizAlis);
+            string sarDövizSatis = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='SAR']/ForexSelling").InnerXml;
+            lblSellingSar.Text = string.Format("{0}", sarDövizSatis);
+
+
+
+
+
+
+
         }
     }
 }
